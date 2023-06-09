8914f032b341814482f408eb2e71906e65b60d47
francisfuzz <15894826+francisfuzz@users.noreply.github.com>
2023-06-09 17:36:05 +0000

    spike: add navigation

--- src/CodeNavPage.js
+++ src/CodeNavPage.js
@@ -1,10 +1,46 @@
-import { Box } from '@primer/react'
+import { UnderlineNav2 as UnderlineNav } from '@primer/react'
+
+import {
+  CodeIcon,
+  CommentDiscussionIcon,
+  GearIcon,
+  GitPullRequestIcon,
+  IssueOpenedIcon,
+  PlayIcon,
+  ProjectIcon,
+  PulseIcon,
+  ShieldIcon,
+} from '@primer/octicons-react'
 
 function CodeNavPage() {
   return (
-    <Box>
-      CodeNavPage
-    </Box>
+    <UnderlineNav aria-label="Repository">
+      <UnderlineNav.Item aria-current="page" icon={CodeIcon}>
+        Code
+      </UnderlineNav.Item>
+      <UnderlineNav.Item icon={IssueOpenedIcon} counter={21}>
+        Issues
+      </UnderlineNav.Item>
+      <UnderlineNav.Item icon={GitPullRequestIcon} counter={3}>
+        Pull Requests
+      </UnderlineNav.Item>
+      <UnderlineNav.Item icon={CommentDiscussionIcon}>Discussions</UnderlineNav.Item>
+      <UnderlineNav.Item icon={PlayIcon}>
+        Actions
+      </UnderlineNav.Item>
+      <UnderlineNav.Item icon={ProjectIcon}>
+        Projects
+      </UnderlineNav.Item>
+      <UnderlineNav.Item icon={ShieldIcon}>
+        Security
+      </UnderlineNav.Item>
+      <UnderlineNav.Item icon={PulseIcon}>
+        Insights
+      </UnderlineNav.Item>
+      <UnderlineNav.Item icon={GearIcon}>
+        Settings
+      </UnderlineNav.Item>
+    </UnderlineNav>
   )
 }
 
