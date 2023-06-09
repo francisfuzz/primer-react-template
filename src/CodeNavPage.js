5d779f341c33012419870073f9ebf868fd8b4f20
francisfuzz <15894826+francisfuzz@users.noreply.github.com>
2023-06-09 17:54:16 +0000

    spike: start FileTree

--- src/CodeNavPage.js
+++ src/CodeNavPage.js
@@ -1,9 +1,11 @@
 import AppHeader from './AppHeader'
+import FileTree from './FileTree'
 
 function CodeNavPage() {
   return (
     <>
       <AppHeader />
+      <FileTree />
     </>
   )
 }
---
+++ src/FileTree.js
@@ -0,0 +1,17 @@
+import { Box } from '@primer/react'
+
+import {
+  SidebarCollapseIcon
+} from '@primer/octicons-react'
+
+function FileTree() {
+  return (
+    (
+      <Box>
+        <SidebarCollapseIcon size={16} />
+      </Box>
+    )
+  )
+}
+
+export default FileTree
